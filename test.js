d54c765d58247e532e995245ee416050f3d1c404
gulen13 <vitaliy.gulen@gmail.com>
2023-02-06 23:17:46 +0200

    training "this"

--- test.js
+++ test.js
@@ -900,3 +900,30 @@
 // };
 
 // console.table(getSortedFriends(users));
+
+const historyService = {
+  orders: [
+    { email: "jacob@hotmail.com", dish: "Burrito" },
+    { email: "solomon@topmail.net", dish: "Burger" },
+    { email: "artemis@coldmail.net", dish: "Pizza" },
+    { email: "solomon@topmail.net", dish: "Apple pie" },
+    { email: "jacob@hotmail.com", dish: "Taco" },
+  ],
+  // Change code below this line
+  getOrdersLog() {
+    return this.orders
+      .map(order => `email: ${order.email} dish: ${order.dish}`)
+      .join(" - ");
+  },
+  getEmails() {
+    const emails = this.orders.map(order => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter(order => order.email === email);
+  },
+  // Change code above this line4
+};
+console.log(historyService.getOrdersByEmail('artemis@coldmail.net'));
+console.log(historyService.getOrdersLog());
