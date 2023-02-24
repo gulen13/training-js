b1d43615908cca02aae1e5c77ec6b3ee4f506343
gulen13 <vitaliy.gulen@gmail.com>
2023-02-24 21:36:22 +0200

    training events

--- test.js
+++ test.js
@@ -1483,15 +1483,125 @@
 
 // function reverseWords(str) {
 
-//   // let arrWords = str.split(' ');
+// let arrWords = str.split(' ');
 
-//   // console.log(arrWords);
+// console.log(arrWords);
 
-//   // let revWords = arrWords.map(word => word.split('').reverse().join(''));
+// let revWords = arrWords.map(word => word.split('').reverse().join(''));
 
-//   // return revWords.join(" ");
+// return revWords.join(" ");
 
 //   return str.split("").reverse().join("").split(" ").reverse().join(" ");
 // }
 
 // console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
+
+// const textInput = document.querySelector(".text-input");
+// const setFocusBtn = document.querySelector('[data-action="set"]');
+// const removeFocusBtn = document.querySelector('[data-action="remove"]');
+
+// setFocusBtn.addEventListener("click", () => {
+//   textInput.focus();
+// });
+
+// removeFocusBtn.addEventListener("click", () => {
+//   textInput.blur();
+// });
+
+// textInput.addEventListener("focus", () => {
+//   textInput.value = "This input has focus";
+// });
+
+// textInput.addEventListener("blur", () => {
+//   textInput.value = "";
+// });
+
+// var array1 = [true, true, true, false,
+//   true, true, true, true,
+//   true, false, true, false,
+//   true, false, false, true,
+//   true, true, true, true,
+//   false, false, true, true];
+
+// function countSheeps(arrayOfSheep) {
+//   return arrayOfSheep.filter(Boolean).length;
+// }
+
+// console.log(countSheeps(array1));
+
+// function betterThanAverage(classPoints, yourPoints) {
+//   let totalPoints = classPoints.reduce((a, b) => { return a + b });
+//   let averagePoints = totalPoints / (classPoints.length);
+//   console.log(totalPoints);
+//   return averagePoints < yourPoints ? true : false;
+// }
+// console.log(betterThanAverage([2, 3], 5));
+
+// function longest(s1, s2) {
+//   // let array = s1.split('');
+
+//   // return array.filter(
+//   //   (a, index, array) => array.indexOf(a) === index
+//   // );
+
+
+
+//   return s1.split('').concat(s2.split('')).sort().filter(
+//     (a, index, array) => array.indexOf(a) === index
+//   ).join('');
+
+
+// }
+// console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+
+// const clearLogBtn = document.querySelector('[data-action="clear"]');
+// const logList = document.querySelector(".log-list");
+// let keypressCounter = 1;
+
+// document.addEventListener("keydown", logMessage);
+// document.addEventListener("keyup", logMessage);
+// clearLogBtn.addEventListener("click", reset);
+
+// function logMessage({ type, key, code }) {
+//   const markup = `<div class="log-item">
+//     <span class="chip">${keypressCounter}</span>
+//     <ul>
+//       <li><b>Event</b>: ${type}</li>
+//       <li><b>Key</b>: ${key}</li>
+//       <li><b>Code</b>: ${code}</li>
+//     </ul>
+//   </div>`;
+
+//   logList.insertAdjacentHTML("afterbegin", markup);
+
+//   if (type === "keyup") {
+//     incrementKeypressCounter();
+//   }
+// }
+// function reset() {
+//   keypressCounter = 1;
+//   logList.innerHTML = "";
+// }
+
+// function incrementKeypressCounter() {
+//   keypressCounter += 1;
+// }
+
+// const select = document.querySelector(".pizza-select");
+// const textOutput = document.querySelector(".text-output");
+// const valueOutput = document.querySelector(".value-output");
+
+// setOutput();
+
+// select.addEventListener("change", setOutput);
+
+// function setOutput() {
+//   const selectedOptionValue = select.value;
+//   const selectedOptionIndex = select.selectedIndex;
+//   const selectedOptionText = select.options[selectedOptionIndex].text;
+
+//   console.log(selectedOptionIndex);
+
+//   textOutput.textContent = selectedOptionText;
+//   valueOutput.textContent = selectedOptionValue;
+// }
